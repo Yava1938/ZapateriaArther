5875f916cebe2fe36a8b9c1a09aa19b5c5cf4ad0
Rodrigo Yael <yava1938@outlook.com>
2020-07-01 02:18:20 -0500

    conexion creada, script de db y base perfil de admin

---
+++ db.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-07-2020 a las 08:49:02
+-- Versión del servidor: 10.1.32-MariaDB
+-- Versión de PHP: 7.2.5
+
+DROP DATABASE   IF     EXISTS `Arther`;
+CREATE DATABASE IF NOT EXISTS `Arther`;
+USE `Arther`;
+
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `Arther`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Administrador`
+--
+
+CREATE TABLE `Administrador` (
+  `Usuario_admin` varchar(10) NOT NULL,
+  `Contrasena_admin` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Inventario`
+--
+
+CREATE TABLE `Inventario` (
+  `Id_Producto` int(11) NOT NULL,
+  `Descripcion_Producto` varchar(15) NOT NULL,
+  `Precio_Producto` float NOT NULL,
+  `Talla_Producto` varchar(10) NOT NULL,
+  `Stock_Producto` int(10) NOT NULL,
+  `Fecha_adquisicion_Producto` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Pedido`
+--
+
+CREATE TABLE `Pedido` (
+  `Id_Pedido` int(11) NOT NULL,
+  `Id_Producto` int(10) NOT NULL,
+  `Cantidad_Producto` int(10) NOT NULL,
+  `Precio_Pedido` double NOT NULL,
+  `Cliente_Pedido` varchar(10) NOT NULL,
+  `Token_Pedido` varchar(25) NOT NULL,
+  `Estado_Pedido` varchar(10) NOT NULL,
+  `Fecha_Pedido` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Inventario`
+--
+ALTER TABLE `Inventario`
+  ADD PRIMARY KEY (`Id_Producto`),
+  ADD KEY `Id_Producto` (`Id_Producto`);
+
+--
+-- Indices de la tabla `Pedido`
+--
+ALTER TABLE `Pedido`
+  ADD PRIMARY KEY (`Id_Pedido`),
+  ADD KEY `Id_Producto` (`Id_Producto`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Inventario`
+--
+ALTER TABLE `Inventario`
+  MODIFY `Id_Producto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `Pedido`
+--
+ALTER TABLE `Pedido`
+  MODIFY `Id_Pedido` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `Inventario`
+--
+ALTER TABLE `Inventario`
+  ADD CONSTRAINT `inventario_ibfk_1` FOREIGN KEY (`Id_Producto`) REFERENCES `Pedido` (`Id_Producto`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
